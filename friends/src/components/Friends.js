85afe214a69b3dcd8386be04c0f256fb27c0a016
Jessica <duellje@oregonstate.edu>
2020-07-21 15:03:56 -0400

    gets friends list from api and renders data to protected route

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const Friends = () => {
+//import utility functions
+import {axiosWithAuth} from '../utils/axiosWithAuth'
+
+const Friends = props => {
+
+    const [friends, addFriends] = useState([]);
+
+    const getFriends = () => {
+        axiosWithAuth()
+            .get('api/friends')
+            .then(res => {
+                addFriends(res.data);
+            })
+            .catch(err => console.log(err));
+    }
+
+    useEffect(() => {
+        getFriends();
+    }, []);
+
+
     return(<div>
         <h1>Friends</h1>
+        {friends.length > 0 ? friends.map(friend => {
+           return (
+                <div key={friend.id}>
+                    <h3>{friend.name}</h3>
+                    <p>Age: {friend.age}</p>
+                    <p>Email: {friend.email}</p>
+                </div>
+            )
+        }) : <p>Loading...</p>}
     </div>)
 }
 
