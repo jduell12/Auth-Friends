e28dc6f0f57e7381dfd5a6b7a72bfdfc833fb38f
Jessica <duellje@oregonstate.edu>
2020-07-21 14:36:30 -0400

    set up login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -8,10 +8,28 @@ const Login = () => {
 
     const [formValues, setValues] = useState(initialFormValues);
 
+    const handleChanges = e => {
+        setValues({...formValues, [e.target.name]: e.target.value})
+    }
+
+    const login = e => {
+        e.preventDefault();
+        console.log('in login');
+        setValues(initialFormValues);
+    }
+
     return(
         <div>
-            <form>
-                <input type="text"  />
+            <form onSubmit={login}>
+                <label htmlFor="username">
+                    Username: &nbsp;
+                    <input id="username" name="username" type="text" value={formValues.username} onChange={handleChanges} />
+                </label>
+                <label htmlFor="password">
+                    Password: &nbsp;
+                    <input id="password" name="password" type="password" value={formValues.password} onChange={handleChanges} />
+                </label>
+                <button>Login</button>
             </form>
         </div>
     )
